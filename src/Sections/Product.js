a4867449d862072a281d17d06dd71d27b11f5e23
amit-sahani <sumitsahani.1855@gmail.com>
2023-11-01 21:13:22 +0530

    new

--- src/Sections/Product.js
+++ src/Sections/Product.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "../App.css";
+
 const Product = () => {
   return (
-    <>
     <div className="cards">
       <div className="page">
-        <img className="img"
+        <img
+          className="img"
+          src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="logo"
+        />
+        <div className="nameTitle">
+          <h3>United Arab Emirate</h3>
+          <h4>7450</h4>
+        </div>
+        <h2>
+          Get on <span className="span">03 Nov, 6:44pm</span>
+        </h2>
+      </div>
+      <div className="page">
+        <img
+          className="img"
           src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="logo"
         />
@@ -19,12 +34,13 @@ const Product = () => {
       </div>
 
       <div className="page">
-      <img className="img"
+        <img
+          className="img"
           src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="logo"
         />
         <div className="nameTitle">
-        <h3>United Arab Emirate</h3>
+          <h3>United Arab Emirate</h3>
           <h4>7450</h4>
         </div>
         <h2>
@@ -33,12 +49,13 @@ const Product = () => {
       </div>
 
       <div className="page">
-      <img className="img"
+        <img
+          className="img"
           src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="logo"
         />
         <div className="nameTitle">
-        <h3>United Arab Emirate</h3>
+          <h3>United Arab Emirate</h3>
           <h4>7450</h4>
         </div>
         <h2>
@@ -46,21 +63,8 @@ const Product = () => {
         </h2>
       </div>
 
-      <div className="page">
-      <img className="img"
-          src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?auto=format&fit=crop&q=80&w=1770&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="logo"
-        />
-        <div className="nameTitle">
-        <h3>United Arab Emirate</h3>
-          <h4>7450</h4>
-        </div>
-        <h2>
-          Get on <span className="span">03 Nov, 6:44pm</span>
-        </h2>
-      </div>
-      </div>
-    </>
+      {/* Repeat this block for other products */}
+    </div>
   );
 };
 
